b5491adfb3c7b77fa952792bb59d391e61e3a4c3
Will White <will@developmentseed.org>
2010-03-19 12:45:23 -0400

    Added mn-feeditem-block template.

---
+++ templates/mn-feeditem-block.tpl.php
@@ -0,0 +1,32 @@
+<?php
+// Render fields with known formatting first.
+$date        = jake_views_render_field($fields['timestamp_1']);
+$feed        = jake_views_render_field($fields['title_1']);
+$title       = jake_views_render_field($fields['title']);
+$description = jake_views_render_field($fields['description']);
+$labels      = jake_views_render_field($fields['data_taxonomy_form']);
+$links       = jake_views_render_field($fields['simpleshare_link']) . jake_views_render_field($fields['mark_trash']);
+
+// All other fields.
+$other       = jake_views_render_field($fields);
+?>
+<div class='feeditem clear-block'>
+  <div class='feeditem-meta clear-block'><?php print $date ?></div>
+  <h2 class='feeditem-title clear-block'><?php print $title ?></h2>
+  <div class='feeditem-feed clear-block'><?php print $feed ?></div>
+
+  <?php if ($description): ?>
+  <div class='feeditem-content prose clear-block'><?php print $description ?></div>
+  <?php endif; ?>
+
+  <?php if ($other): ?>
+    <div class='feeditem-other clear-block'><?php print $other ?></div>
+  <?php endif; ?>
+
+  <?php if ($labels): ?>
+    <div class='feeditem-labels clear-block'><?php print $labels ?></div>
+  <?php endif; ?>
+
+  <div class='feeditem-links clear-block'><?php print $links ?></div>
+
+</div>
