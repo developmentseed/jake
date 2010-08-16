a55984b8cbf2eb1a3f1ab9983f462499aba36c94
Will White <will@developmentseed.org>
2010-08-16 14:43:59 -0400

    Added color picker for map backgrounds.

--- templates/designkit.tpl.php
+++ templates/designkit.tpl.php
@@ -94,4 +94,12 @@ div.sidebar div.view-mn-search-saved div.views-field-title a,
   color:#fff;
   }
 
+/**
+ * Map color ==========================================================
+ */
+body.views-mode-map div.openlayers-views-map,
+body.views-mode-map div.openlayers-views-map div.openlayers-map {
+  background-color:<?php print $map_color ?>;
+}
+
 </style>
