26774f3dd2560d1a6b7eda3b7d46f7457624eddd
Jeff Miccolis <jeff@miccolis.net>
2010-09-07 10:26:42 -0400

    More specific selector in the trash mark js, so that other marks can be used.

--- js/theme.js
+++ js/theme.js
@@ -51,7 +51,7 @@ Drupal.behaviors.jake = function (context) {
     });
   });
 
-  $('.mark-link > a').bind('mark.drupalMark', function() { $(this).parents('li.views-row').hide(600);});
+  $('.views-field-mark-trash .mark-link > a').bind('mark.drupalMark', function() { $(this).parents('li.views-row').hide(600);});
 
   // Change the z-index of the l10n client when it is expanded
   Drupal.jake.checkl10n();
